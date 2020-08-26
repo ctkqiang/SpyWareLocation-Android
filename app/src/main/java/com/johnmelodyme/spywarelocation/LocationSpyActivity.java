23553904e39852cd078b0156d8b51251c7094d41
John Melody Me <Johnmelodyme@yandex.com>
2020-08-26 16:46:53 +0800

    Added LOcation Permission

--- app/src/main/java/com/johnmelodyme/spywarelocation/LocationSpyActivity.java
+++ app/src/main/java/com/johnmelodyme/spywarelocation/LocationSpyActivity.java
@@ -1,18 +1,56 @@
 package com.johnmelodyme.spywarelocation;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.johnmelodyme.spywarelocation.util.logger;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class LocationSpyActivity extends AppCompatActivity {
     private static final String Application = "LocationSpyActivity";
+    private Intent OSlocationetting;
     private TextView _display_Location;
     private logger logger;
 
-    private void _UIComponent() {
-        _display_Location = findViewById(R.id.displayLocation);
-        logger.tell("UI Component Loaded");
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Toast.makeText(LocationSpyActivity.this, "Location Permission Granted",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                if (permissionDeniedResponse.isPermanentlyDenied()) {
+                    logger.dev("User DeniedTHe Permission");
+                    _GotoLocationSetting();
+                    logger.tell("Prompt To Location Setting");
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+
+        }).check();
     }
 
     @Override
@@ -22,6 +60,15 @@ public class LocationSpyActivity extends AppCompatActivity {
 
         // Load UIComponent
         _UIComponent();
+    }
 
+    private void _UIComponent() {
+        _display_Location = findViewById(R.id.displayLocation);
+        logger.tell("UI Component Loaded");
+    }
+
+    private void _GotoLocationSetting() {
+        OSlocationetting = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(OSlocationetting);
     }
 }
